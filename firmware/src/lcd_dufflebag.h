f0e21ed204b0b53c6f208de2d968d82891f36e79
Sean Caulfield <sean@yak.net>
2015-04-19 22:23:56 -0400

    I MADE IT SAY DICKBUTT YEAH (after hours of yak shaving this is, in fact, progress)

--- firmware/src/lcd_dufflebag.h
+++ firmware/src/lcd_dufflebag.h
@@ -3,37 +3,49 @@
 
 #include<Arduino.h>
 
+#if 0
 //#define Wire TinyWireS
 
-//#ifndef TWI_RX_BUFFER_SIZE
-//#define TWI_RX_BUFFER_SIZE (16)
-//#endif
+#ifndef TWI_RX_BUFFER_SIZE
+#define TWI_RX_BUFFER_SIZE (16)
+#endif
 
 // Address where the i2c address should be stored in EEPROM
-//#define EEPROM_I2C_ADDR 0x00
+#define EEPROM_I2C_ADDR 0x00
 
 // Default i2c address if one has not been stored yet
-//#define DEFAULT_I2C_ADDR 0x42
+#define DEFAULT_I2C_ADDR 0x42
 
-//#define REG_LINE1  (0x00)
-//#define REG_LINE2  (0x10)
-//#define REG_BRIGHT (0x11)
-//#define REG_MAX    (0x12)
+#define REG_LINE1  (0x00)
+#define REG_LINE2  (0x10)
+#define REG_BRIGHT (0x11)
+#define REG_MAX    (0x12)
+#endif
 
 // Pin mappings
-#define RS  (PB2)
-#define RW  (PB1)
-#define E   (PB0)
+#define RS  (8)
+#define RW  (9)
+#define E   (10)
 
-#define DB4 (PA0)
-#define DB5 (PA1)
-#define DB6 (PA2)
-#define DB7 (PA3)
+#define DB4 (0)
+#define DB5 (1)
+#define DB6 (2)
+#define DB7 (3)
 
-#define BACKLIGHT (PA5)
+#define BACKLIGHT (7)
+
+#define RX  (6)
+#define TX  (5)
+#define CLK (4)
 
 // LCD parameters
 #define COLS (8)
 #define ROWS (2)
+
+// Serial commands
+#define UART_CMD_MIN    (0x7F)
+#define UART_CMD_HOME   (0xFD)
+#define UART_CMD_CLEAR  (0xFE)
+#define UART_CMD_RESET  (0xFF)
 
 #endif
--- firmware/src/lcd_dufflebag.ino
+++ firmware/src/lcd_dufflebag.ino
@@ -3,122 +3,73 @@
  */
 
 #include <Arduino.h>
-#include <LiquidCrystal.h>
-#include <TinyWireS.h>
 #include <EEPROM.h>
+#include <LiquidCrystal.h>
+#include <SoftwareSerial.h>
 #include "lcd_dufflebag.h"
 
-byte my_i2c_addr = 0;
-byte my_reg_addr = 0;
 byte brightness = 0x7f;
 
-//byte display_buff[32];
-
+SoftwareSerial nouart(RX, TX);
 LiquidCrystal lcd = LiquidCrystal(RS, RW, E, DB4, DB5, DB6, DB7);
 
-/*
-void recv_handler(uint8_t num_bytes) {
+void display_setup() {
 
-  // TODO Somehow inform user of buffer overflow?
-  if (num_bytes < 1 || num_bytes > TWI_RX_BUFFER_SIZE) {
-    return;
-  }
-
-  byte a = TinyWireS.receive();
-  num_bytes--;
-
-  if (a < 0 || a >= REG_MAX) {
-    return;
-  } else {
-    my_reg_addr = a;
-  }
-
-  // If there's only a single byte, that was an address set, so go ahead and
-  // return
-  if (!num_bytes) {
-    return;
-  }
-
-  //
-  // TODO Dispatch code based on address. Cases to handle:
-  //
-  // 1. Read/write display buffer. If write, set some flag to check to push the
-  // data to the display next chance we get in the main loop.
-  //
-  // 2. Read/write brightness level. Same flag deal (may need to use single
-  // flag due to memory limitations?)
-  //
-  // 3. Read/write backlight color (RGB). Same flag deal (may need to use
-  // single flag due to memory limitations?)
-  //
-  // 4. (Optional) Read/write i2c address.
-  // 5. (Optional) Read/write display size (maybe just read?)
-  // 6. (Optional) Pass-through to LCD controller somehow? Could be useful to
-  // upload character glyphs, for example.
-  //
-
-  // IDEA: Discard brightness, just deal in RGB.
-
-#if 0
-  // Otherwise, handle further reads based on address specified
-  if (my_reg_addr >= REG_LINE1 && my_reg_addr < REG_BRIGHT) {
-    while (num_bytes-- && my_reg_addr >= REG_LINE1 && my_reg_addr < REG_BRIGHT) {
-      byte b = TinyWireS.receive();
-      display_buff[
-    }
-  }
-#endif
-
-}
-
-void req_handler() {
-
-  // Allow master to read display buffer
-  if (my_reg_addr >= REG_LINE1 && my_reg_addr < REG_BRIGHT) {
-    TinyWireS.send(display_buff[my_reg_addr - REG_LINE1]);
-
-  // Allow master to read display brightness level
-  } else if (my_reg_addr == REG_BRIGHT) {
-    TinyWireS.send(brightness);
-  }
-
-  // Increment register pointer, wrapping around if we're at the end
-  my_reg_addr++;
-  if (my_reg_addr >= REG_MAX) {
-    my_reg_addr = 0x00;
-  }
-
-}
-*/
-
-void setBacklight() {
   pinMode(BACKLIGHT, OUTPUT);
   analogWrite(BACKLIGHT, brightness);
+
+  lcd.begin(COLS, ROWS);
+  lcd.print("DICKBUTT");
+  delay(300);
+  lcd.clear();
+
+}
+
+void cmd_clear() {
+  lcd.clear();
+}
+
+void cmd_home() {
+  lcd.home();
+}
+
+void cmd_reset() {
+  display_setup();
 }
 
 void setup() {
 
-  // Get i2c address from EEPROM
-  //my_i2c_addr = EEPROM.read(EEPROM_I2C_ADDR);
-  //if (my_i2c_addr > 127) {
-  //  my_i2c_addr = DEFAULT_I2C_ADDR;
-  //  EEPROM.write(EEPROM_I2C_ADDR, my_i2c_addr);
-  //}
+  display_setup();
 
-  //Wire.begin(my_i2c_addr);
-  //Wire.onRequest(req_handler);
-  //Wire.onReceive(recv_handler);
-
-  pinMode(BACKLIGHT, OUTPUT);
-  
-  lcd.begin(COLS, ROWS);
-  lcd.print("DICKBAGS");
+  nouart.begin(9600);
 
 }
 
 void loop() {
-  lcd.setCursor(0, 1);
-  lcd.print(millis());
-  setBacklight();
-  delay(1000);
+  
+  // Poll for new data
+  if (nouart.available()) {
+    byte c = nouart.read();
+
+    // Check for and handle magical command bytes. Should make this a proper
+    // state machine.
+    if (c >= UART_CMD_MIN) {
+      switch (c) {
+        case UART_CMD_HOME:
+          break;
+        case UART_CMD_CLEAR:
+          break;
+        case UART_CMD_RESET:
+          break;
+        default:
+          break;
+      }
+
+    // Default is to write a character to the display
+    } else {
+      lcd.write(c);
+    }
+
+  }
+
 }
